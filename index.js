21fac6258c154fc2d58ce2a1e6f2942377b7d60f
AgoraSoftware <contato@agorasoftware.com.br>
2021-05-12 23:11:41 -0300

    Alteração de palavra World escrita com erro

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('expresss');
 const app = express
 
 app.get('/teste', (req, res) => {
-  return res.json({ hello: 'hord' })
+  return res.json({ hello: 'World' })
 });
 
 app.listen(3333);
